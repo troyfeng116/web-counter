1223bea82eba33baf6788a9fcaf0e83df6b0fcdb
Troy Feng <troy.feng@yale.edu>
2021-01-04 12:01:42 -0500

    Add increment set

--- src/Components/Counter/Counter.tsx
+++ src/Components/Counter/Counter.tsx
@@ -6,26 +6,33 @@ import { TextField } from '../TextField/TextField'
 
 export const Counter: React.FC = () => {
     const [count, setCount] = useState<number>(0)
-    const [resetCount, setResetCount] = useState<string>('')
+    const [newCountText, setNewCountText] = useState<string>('')
+    const [incrementText, setIncrementText] = useState<string>('1')
+    const [increment, setIncrement] = useState<number>(1)
+
+    const parseIntWithNaN = (s: string): number => {
+        const n = parseInt(s)
+        return isNaN(n) ? 0 : n
+    }
 
     return (
         <div className="w-96 bg-white mx-auto shadow flex flex-col items-center mb-6 p-6">
             <div className="w-full bg-gray-200 py-1 px-4 text-3xl text-right mb-3 rounded-lg">{count}</div>
             <div className="w-full flex items-center justify-between mb-9">
-                <Button title="-" size={ButtonSize.Small} variant={ButtonVariant.Minus} onClick={() => setCount(count - 1)} />
-                <Button title="+" size={ButtonSize.Small} variant={ButtonVariant.Plus} onClick={() => setCount(count + 1)} />
+                <Button title="-" size={ButtonSize.Small} variant={ButtonVariant.Minus} onClick={() => setCount(count - increment)} />
+                <Button title="+" size={ButtonSize.Small} variant={ButtonVariant.Plus} onClick={() => setCount(count + increment)} />
             </div>
             <div className="w-full mb-3">
                 <Button title="Reset counter" size={ButtonSize.Medium} variant={ButtonVariant.Grey} onClick={() => setCount(0)} fullWidth={true} />
             </div>
             <div className="flex justify-center items-center w-full bg-gray-200 py-1 px-4 mb-3 rounded">
-                <div className="mr-4">Set counter to:</div>
+                <div className="mr-4">Set to:</div>
                 <TextField
                     placeholder=""
-                    value={resetCount}
+                    value={newCountText}
                     onChange={(e) => {
                         const regex = /^\d*$/
-                        if (e.target.value.match(regex)) setResetCount(e.target.value)
+                        if (e.target.value.match(regex)) setNewCountText(e.target.value)
                     }}
                     size={8}
                 />
@@ -35,8 +42,31 @@ export const Counter: React.FC = () => {
                     size={ButtonSize.Small}
                     variant={ButtonVariant.Grey}
                     onClick={() => {
-                        const x = parseInt(resetCount)
-                        setCount(isNaN(x) ? 0 : x)
+                        setCount(parseIntWithNaN(newCountText))
+                    }}
+                />
+            </div>
+            <div className="flex justify-center items-center w-full bg-gray-200 py-1 px-4 mb-3 rounded">
+                <div className="mr-4">Increment:</div>
+                <TextField
+                    placeholder=""
+                    value={incrementText}
+                    onChange={(e) => {
+                        const regex = /^\d*$/
+                        if (e.target.value.match(regex)) {
+                            setIncrementText(e.target.value)
+                        }
+                    }}
+                    size={5}
+                />
+                <div className="w-2" />
+                <Button
+                    title="Set"
+                    size={ButtonSize.Small}
+                    variant={ButtonVariant.Grey}
+                    onClick={() => {
+                        const x = parseIntWithNaN(incrementText)
+                        setIncrement(x)
                     }}
                 />
             </div>
