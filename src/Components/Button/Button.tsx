c02a03980e8ec49c53b8bd898e86d4b8f9641fd0
Troy Feng <troy.feng@yale.edu>
2021-01-04 10:29:40 -0500

    Simple counter mechanic

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -10,6 +10,8 @@ export enum ButtonVariant {
     Green = 'Green',
     Red = 'Red',
     Grey = 'Grey',
+    Plus = 'Plus',
+    Minus = 'Minus',
 }
 
 export const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
@@ -30,10 +32,19 @@ export const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
             baseClassName = 'bg-gray-400 text-white'
             hoverClassName = 'hover:bg-gray-500'
             break
+        case ButtonVariant.Plus:
+            baseClassName = 'bg-gray-600 text-green-300 text-3xl py-0 px-8'
+            hoverClassName = 'hover:bg-gray-700'
+            break
+        case ButtonVariant.Minus:
+            baseClassName = 'bg-gray-600 text-red-300 text-3xl py-0 px-8'
+            hoverClassName = 'hover:bg-gray-700'
+            break
     }
+
     return (
         <div
-            className={`py-2 px-4 cursor-pointer rounded shadow ${baseClassName} ${hoverClassName}`}
+            className={`py-2 px-4 cursor-pointer rounded shadow font-bold ${baseClassName} ${hoverClassName}`}
             onClick={onClick}
         >
             {title}
--- src/Components/Counter/Counter.tsx
+++ src/Components/Counter/Counter.tsx
@@ -1,5 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button, { ButtonVariant } from '../Button'
 
 export const Counter: React.FC = () => {
-    return <div className="w-96 bg-white mx-auto shadow text-center mb-4">Counter</div>
+    const [count, setCount] = useState<number>(0)
+
+    return <div className="w-96 bg-white mx-auto shadow flex flex-col items-center mb-6 p-6">
+        <div className="w-full bg-gray-200 py-1 px-4 text-3xl text-right mb-3 rounded-lg">{count}</div>
+        <div className="w-full flex items-center justify-between">
+            <Button title="-" variant={ButtonVariant.Minus} onClick={() => setCount(count - 1)} />
+            <Button title="+" variant={ButtonVariant.Plus} onClick={() => setCount(count + 1)} />
+        </div>
+    </div>
 }
