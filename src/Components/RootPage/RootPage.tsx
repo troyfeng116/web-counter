937ec15b3e23247ccff84b5b8f15f6f42194968d
Troy Feng <troy.feng@yale.edu>
2021-01-05 11:20:03 -0500

    Cleanup in RootPage

--- src/Components/RootPage/RootPage.tsx
+++ src/Components/RootPage/RootPage.tsx
@@ -8,19 +8,20 @@ export const RootPage: React.FC = () => {
     const [counterIds, setCounterIds] = useState<string[]>([generateId(10)])
 
     const deleteCounterAt = (index: number): void => {
+        if (counterIds.length <= 1) return
         const newCounterIds = counterIds.slice(0, index).concat(counterIds.slice(index + 1))
         setCounterIds(newCounterIds)
     }
 
     return (
         <div className="bg-blue-100 min-h-screen p-12 flex flex-col items-center">
-            <header className="mb-12 text-4xl uppercase font-bold">Online Counter</header>
+            <header className="mb-8 text-4xl uppercase font-bold">Online Counter</header>
             <div className="w-full flex flex-col items-center">
                 {counterIds.map((id, index) => (
                     <Counter key={id} deleteThisCounter={() => deleteCounterAt(index)} />
                 ))}
                 <Button
-                    title="New counter"
+                    title="+ Add new counter"
                     variant={ButtonVariant.Green}
                     size={ButtonSize.Large}
                     onClick={() => {
